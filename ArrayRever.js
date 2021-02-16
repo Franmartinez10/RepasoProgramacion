56cee29d9deec8e3361e43de1498975370e7e7bf
Fran Martinez <fran.martinez.torres@gmail.com>
2021-02-16 17:30:24 +0100

    primera versión

--- ArrayRever.js
+++
@@ -1,9 +0,0 @@
-function arrayInvers(array) {
-    var result = [];
-    for (var i = array.length - 1; i >= 0; i--) {
-        result.push(array[i]);
-    }
-    return result;
-}
-var arraito = [15, 2, 63, 74, 5, 776, 7879, 8654];
-console.log(arrayInvers(arraito));
--- numImpares.ts
+++
@@ -1,8 +0,0 @@
-function numimpares(num:number)
-{   let i=1
-    while(i<=num)
-    {
-    console.log(i+=2)
-    }
-}
-numimpares(12)
